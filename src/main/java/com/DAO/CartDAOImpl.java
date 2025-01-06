eec04844d6f34922a769898463ad7afb46e6fd9f
Chử Quốc Chính <93875719+CihK0ng@users.noreply.github.com>
2025-01-06 16:54:03 +0700

    Update CartDAOImpl.java

--- src/main/java/com/DAO/CartDAOImpl.java
+++ src/main/java/com/DAO/CartDAOImpl.java
@@ -1,9 +1,36 @@
 package com.DAO;
 
-
+import com.entity.Cart;
 
 public class CartDAOImpl implements CartDAO{
-  public 
+
+  private Connection conn;
+  public CartDAOImpl(Connection conn){
+    this.conn = conn;
+    
+  }
+
+
+  
+
+
+  
+  public boolean addCart(Cart c){
+    boolean f = false;
+    try {
+      String sql = "insert into cart()";
+      
+    } catch ( Exception c){
+      c.printStackTrace();
+    
+    }
+    
+    
+    
+    
+    return false;
+  }
+
   
 
 
